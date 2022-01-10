15acd997cbdc91cae0e9c080d8ab5cac0fdc5c14
airbass <artur.basinski@gmail.com>
2022-01-10 01:02:01 +0100

    Final Version

--- src/app/times.directive.ts
+++ src/app/times.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appTimes]'
+  selector: '[appTimes]',
 })
 export class TimesDirective {
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private tempalteRef: TemplateRef<any>
+  ) {}
+  @Input('appTimes') set render(times: number) {
+    this.viewContainer.clear();
 
-  constructor() { }
-
+    for (let i = 0; i < times; i++) {
+      this.viewContainer.createEmbeddedView(this.tempalteRef, {});
+    }
+  }
 }
